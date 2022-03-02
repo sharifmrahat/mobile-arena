02656ed31c3c239a87b862952f90e2a83a6fc6df
Sazzad <imr4h47@gmail.com>
2022-03-02 13:58:45 +0600

    object is updated

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-//-------Getting element by ID----------------\\
+//------------------Getting element by ID----------------\\
 
 const inputField = document.getElementById("input-field");
 const alertMsg = document.getElementById("alert");
@@ -7,12 +7,12 @@ const searchText = document.getElementById("search-text");
 const searchResult = document.getElementById("search-result");
 const modalBody = document.getElementById("modal-body");
 
-//------------Alert Msg & Spinner hidden by default------------\\
+//------------Spinner & Alert is hidden by default-----------\\
 
 alertMsg.style.display = "none";
 spinner.setAttribute("hidden", true);
 
-//-----------Click event handler on search button----------------\\
+//-----------Event Handler Onclick Search button----------------\\
 
 document.getElementById("search-btn").addEventListener("click", function () {
     const inputText = inputField.value;
@@ -24,7 +24,7 @@ document.getElementById("search-btn").addEventListener("click", function () {
     fetch(url)
         .then((res) => res.json())
         .then((data) => {
-            //------condition if search item not found--------\\
+            //------condition if searched item not found--------\\
             if (data.status === false) {
                 searchResult.innerHTML = "";
                 alertMsg.style.display = "block";
@@ -43,6 +43,7 @@ document.getElementById("search-btn").addEventListener("click", function () {
 });
 
 //---------Function call after data load from API------------\\
+
 const getPhones = (items) => {
     searchResult.innerHTML = "";
     let allItems;
@@ -51,46 +52,40 @@ const getPhones = (items) => {
     } else {
         allItems = items;
     }
-    // console.log("allItems: ", allItems);
 
-    //-------Loop through each item and set innerHTML for div>card----------\\
+    //-------Loop through each item---------\\
+
     allItems.forEach((phone) => {
-        // console.log(phone)
-
         const div = document.createElement("div");
         div.classList.add("col");
         div.innerHTML = `
-    <div class="card text-center shadow">
-    <img src="${phone.image}" class="card-img-top image-fluid w-50 mx-auto pt-4" alt="">
-    <div class="card-body mb-3">
-        <h4 class="card-title text-imperial fw-bold">${phone.phone_name}</h4>
-        <h5 class="card-text text-muted fs-4">${phone.brand}</h5>
+                    <div class="card h-100 text-center shadow">
+                            <img src="${phone.image}" class="card-img-top image-fluid w-50 mx-auto pt-4" alt="">
+                        <div class="card-body mb-3">
+                            <h4 class="card-title text-imperial fw-bold">${phone.phone_name}</h4>
+                            <h5 class="card-text text-muted fs-4">${phone.brand}</h5>
+                        </div>
+                        <div class="mb-4">
+                            <button onclick="getDetails('${phone.slug}')" class="btn bg-amour text-light fs-5 fw-bold" data-bs-toggle="modal"
+                            data-bs-target="#cardModal">Details</button>
+                        </div>
 
-    </div>
-    <div class="mb-4">
-        <button onclick="getDetails('${phone.slug}')" class="btn bg-amour text-light fs-5 fw-bold" data-bs-toggle="modal"
-            data-bs-target="#cardModal">Details</button>
-    </div>
-
-</div>
-    `;
+                    </div>`;
         searchResult.appendChild(div);
     });
 };
 
-//-------Reload data and getting details after clicking button---------\\
+//---------Getting Phone details after clicking Details button---------\\
 const getDetails = (phoneId) => {
     const slugUrl = `https://openapi.programming-hero.com/api/phone/${phoneId}`;
 
-    //-----fetching data for individual item----------\\
-
+    //-----fetching data for individula slug----------\\
     fetch(slugUrl)
         .then((res) => res.json())
         .then((data) => showDetails(data.data));
 };
 
 const showDetails = (phone) => {
-    console.log(phone.others);
     modalBody.innerHTML = `
             <div class="col-md-4 ">
                     <img src="${phone.image}"
@@ -100,10 +95,10 @@ const showDetails = (phone) => {
                 <div class="card-body">
                     <h4 class="card-title text-amour fw-bold">${phone.name}</h3>
                     <p><b>Brand: </b>${phone.brand}</p>
-                    <p><b>Release Date: </b>${phone.releaseDate || "Release date is unknown"}</p>
-                    <p><b>Specification: </b>Display: ${phone.mainFeatures.displaySize}, Memory: ${phone.mainFeatures.memory}, Storage: ${phone.mainFeatures.storage}, Chipset: ${phone.mainFeatures.chipSet}.</p>
+                    <p><b>Release Date: </b>${phone?.releaseDate || "Release date is unknown"}</p>
+                    <p><b>Specification: </b>Display: ${phone?.mainFeatures?.displaySize}, Memory: ${phone?.mainFeatures?.memory}, Storage: ${phone?.mainFeatures?.storage}, Chipset: ${phone?.mainFeatures?.chipSet}.</p>
                     <p><b>Sensors: </b>${phone.mainFeatures.sensors}</p>
-                    <p><b>Others: </b>${phone?.others?.Bluetooth || "Other features is unknown"}</p>
+                    <p><b>Others: </b>WLAN: ${phone?.others?.WLAN || "WLAN info is unknown"}, Bluetooth: ${phone?.others?.Bluetooth || "Bluetooth version is unknown"}, GPS: ${phone?.others?.GPS || "GPS info is unknown"}, NFC: ${phone?.others?.NFC || "NFC info is unknown"}, USB: ${phone?.others?.USB || "USB types is unknown"}</p>
                 </div>
             </div>`;
 };
